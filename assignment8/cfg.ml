b57c5518a9f07124391be1a747a09b3897d1fa24
Robin Xia <rx2141@nyu.edu>
2023-05-05 13:36:22 -0400

    finished

--- assignment8/cfg.ml
+++ assignment8/cfg.ml
@@ -31,6 +31,15 @@ let call_kill_list = ["$1";"$2";"$3";"$4";"$5";"$6";"$7";"$8";"$9";"$10";
 
 module IUGraph = Graph.UndirectedGraph(IGraphNode)
 
+let clear_file =
+  let oc = open_out "log.txt" in
+  close_out oc
+
+let write_to_file (str: string) : unit =
+  let oc = open_out_gen [Open_wronly; Open_creat; Open_append] 0o666 "log.txt" in
+  output_string oc str;
+  close_out oc
+
 (* this is a wrapper to addEdge that prevents adding self edges.
    to do all sorts of other complicated stuff for eg coloring *)
 let specialAddEdge u v g =
@@ -60,10 +69,364 @@ let string_of_igraph (g: interfere_graph) : string =
 
 (*******************************************************************)
 (* PS7 TODO:  interference graph construction *)
+(*graph to represent the connections between blocks*)
+module IDGraph = Graph.DirectedGraph(IGraphNode)
+type block_graph = IDGraph.graph
+
+module NodeHashTable = Hashtbl.Make(String)
+
+let node_hash_table : NodeSet.t NodeHashTable.t = NodeHashTable.create 32
+(*
+let add_to_node_set ht key v =
+  match NodeHashTable.find_opt ht key with
+  | None -> NodeHashTable.add ht key (NodeSet.singleton v)
+  | Some set -> NodeHashTable.replace ht key (NodeSet.add v set)
+*)
+let add_to_node_set ht key set =
+  NodeSet.iter (fun v ->
+    match NodeHashTable.find_opt ht key with
+    | None -> NodeHashTable.add ht key (NodeSet.singleton v)
+    | Some existing_set -> NodeHashTable.replace ht key (NodeSet.add v existing_set)
+(*
+    | Some existing_set -> NodeHashTable.remove ht key;
+                            NodeHashTable.add ht key (NodeSet.add v existing_set)
+*)
+
+  ) set
+
+let remove_from_node_set ht key v =
+  match NodeHashTable.find_opt ht key with
+  | None -> ()
+  | Some set -> NodeHashTable.replace ht key (NodeSet.remove v set)
+
+let update_node_set ht key old_v new_v =
+  match NodeHashTable.find_opt ht key with
+  | None -> ()
+  | Some set ->
+    let new_set = NodeSet.remove old_v (NodeSet.add new_v set) in
+    NodeHashTable.replace ht key new_set
+
+let find_in_node_set ht key =
+  match NodeHashTable.find_opt ht key with
+  | None -> NodeSet.empty
+  | Some set -> set
+
+
+let copy_nodeset (set1 : NodeSet.t ref) (set2 : NodeSet.t ref) : unit =
+    (*copy set1 to set2*)
+  set2 := NodeSet.empty; (* clear set2 *)
+  NodeSet.iter (fun x -> set2 := NodeSet.add x !set2) !set1 (* add elements from set1 to set2 *)
+
+
+let node_sets_equal set1 set2 =
+  let sorted_list1 = List.sort compare (NodeSet.elements set1) in
+  let sorted_list2 = List.sort compare (NodeSet.elements set2) in
+  List.compare_lengths sorted_list1 sorted_list2 = 0 &&
+  List.for_all2 (fun n1 n2 -> compare n1 n2 = 0) sorted_list1 sorted_list2
+
+let string_of_igraph2 (g: block_graph) : string =
+  let rec string_of_row (n: IDGraph.node) =
+    let ns = IDGraph.succ n g in
+    Printf.sprintf "%s : {%s}"
+      (string_of_node n)
+      (String.concat "," (List.map string_of_node (NodeSet.elements ns)))
+  in
+  let rows = String.concat "\n" (List.map string_of_row (NodeSet.elements (IDGraph.nodes g))) in
+  Printf.sprintf "{\n%s\n}" rows
+
+
+let find_label (block: block) : label =
+  let rec helper block =
+    match block with
+    | Label l :: _ -> l
+    | [] -> write_to_file("\n Fatal error 0001\n"); raise Implement_Me
+    | _ :: rest -> helper rest
+  in
+  helper block
+
+let label2block (f: func) (label : string) : block =
+    List.find (fun b -> (find_label b) = label ) f
+
+
+let rec find_jump_labels block =
+  match block with
+  | [] -> []
+  | Jump(l) :: rest -> l :: find_jump_labels rest
+  | If(_, _, _, l1, l2) :: rest -> l1 :: l2 :: find_jump_labels rest
+  | _ :: rest -> find_jump_labels rest
+
+
+
+
+let reg_node v =
+  match v with
+  | 0 -> RegNode(Mips.R0)
+  | 1 -> RegNode(Mips.R1)
+  | 2 -> RegNode(Mips.R2)
+  | 3 -> RegNode(Mips.R3)
+  | 4 -> RegNode(Mips.R4)
+  | 5 -> RegNode(Mips.R5)
+  | 6 -> RegNode(Mips.R6)
+  | 7 -> RegNode(Mips.R7)
+  | 8 -> RegNode(Mips.R8)
+  | 9 -> RegNode(Mips.R9)
+  | 10 -> RegNode(Mips.R10)
+  | 11 -> RegNode(Mips.R11)
+  | 12 -> RegNode(Mips.R12)
+  | 13 -> RegNode(Mips.R13)
+  | 14 -> RegNode(Mips.R14)
+  | 15 -> RegNode(Mips.R15)
+  | 16 -> RegNode(Mips.R16)
+  | 17 -> RegNode(Mips.R17)
+  | 18 -> RegNode(Mips.R18)
+  | 19 -> RegNode(Mips.R19)
+  | 20 -> RegNode(Mips.R20)
+  | 21 -> RegNode(Mips.R21)
+  | 22 -> RegNode(Mips.R22)
+  | 23 -> RegNode(Mips.R23)
+  | 24 -> RegNode(Mips.R24)
+  | 25 -> RegNode(Mips.R25)
+  | 26 -> RegNode(Mips.R26)
+  | 27 -> RegNode(Mips.R27)
+  | 28 -> RegNode(Mips.R28)
+  | 29 -> RegNode(Mips.R29)
+  | 30 -> RegNode(Mips.R30)
+  | 31 -> RegNode(Mips.R31)
+  | _ ->  write_to_file("\n Fatal error 0002\n"); raise Implement_Me
+
+
+let reg_node_of_string str =
+  match str with
+  | "$0" -> RegNode(Mips.R0)
+  | "$1" -> RegNode(Mips.R1)
+  | "$2" -> RegNode(Mips.R2)
+  | "$3" -> RegNode(Mips.R3)
+  | "$4" -> RegNode(Mips.R4)
+  | "$5" -> RegNode(Mips.R5)
+  | "$6" -> RegNode(Mips.R6)
+  | "$7" -> RegNode(Mips.R7)
+  | "$8" -> RegNode(Mips.R8)
+  | "$9" -> RegNode(Mips.R9)
+  | "$10" -> RegNode(Mips.R10)
+  | "$11" -> RegNode(Mips.R11)
+  | "$12" -> RegNode(Mips.R12)
+  | "$13" -> RegNode(Mips.R13)
+  | "$14" -> RegNode(Mips.R14)
+  | "$15" -> RegNode(Mips.R15)
+  | "$16" -> RegNode(Mips.R16)
+  | "$17" -> RegNode(Mips.R17)
+  | "$18" -> RegNode(Mips.R18)
+  | "$19" -> RegNode(Mips.R19)
+  | "$20" -> RegNode(Mips.R20)
+  | "$21" -> RegNode(Mips.R21)
+  | "$22" -> RegNode(Mips.R22)
+  | "$23" -> RegNode(Mips.R23)
+  | "$24" -> RegNode(Mips.R24)
+  | "$25" -> RegNode(Mips.R25)
+  | "$26" -> RegNode(Mips.R26)
+  | "$27" -> RegNode(Mips.R27)
+  | "$28" -> RegNode(Mips.R28)
+  | "$29" -> RegNode(Mips.R29)
+  | "$30" -> RegNode(Mips.R30)
+  | "$31" -> RegNode(Mips.R31)
+  | _ ->  write_to_file("\n Fatal error 0002\n"); raise Implement_Me
 
 (* given a function (i.e., list of basic blocks), construct the
  * interference graph for that function.  This will require that
  * you build a dataflow analysis for calculating what set of variables
  * are live-in and live-out for each program point. *)
-let build_interfere_graph (f : func) : interfere_graph = 
-    raise Implement_Me
+let build_interfere_graph (f : func) : interfere_graph =
+    (*Print blocks to log file*)
+    clear_file;
+    let print_block block =
+      let str = block2string block in
+      write_to_file (str^"\n") in
+    List.iter print_block f;
+
+    (*Build direct graph of blocks*)
+    let build_block_graph (f : func) : block_graph =
+        (*Get a list of label*)
+        let result_block_graph = IDGraph.empty in
+        let label_list = List.map find_label f in
+        (*adj_label_list is a list of list, i th element is a
+          list of labels which can be jump to from the i th label in label_list
+        *)
+        let adj_label_list = List.map find_jump_labels f in
+        (*List.iter (fun lst -> List.iter write_to_file lst) adj_label_list;*)
+        (*List.iter write_to_file label_list;*)
+        let ref_g = ref result_block_graph in
+        (*add all nodes to graph*)
+        List.iter (fun label ->
+            let new_node = VarNode(label) in
+            ref_g := IDGraph.addNode new_node !ref_g
+            ) label_list;
+        (*build connections *)
+        List.iter2 (fun label adj_label ->
+            let src_node = VarNode(label) in
+            List.iter (fun dst_label ->
+                let dst_node = VarNode(dst_label) in
+                            ref_g := IDGraph.addEdge src_node dst_node !ref_g
+                ) adj_label;
+         ) label_list adj_label_list;
+        !ref_g
+    in
+    let result_graph = IUGraph.empty in
+    let result_block_graph = build_block_graph f in
+    let log_msg = string_of_igraph2 result_block_graph in
+    write_to_file ("\n block graph starts: \n"^log_msg^"\n");
+    (*Choose the block which has lowest out degree as starting point*)
+    let get_start_block (g : block_graph) : igraph_node =
+        let nodes = (NodeSet.elements (IDGraph.nodes g)) in
+        let node_with_least_succ (g : block_graph)  nodes =
+          let succ_counts = List.map (fun n -> List.length (NodeSet.elements (IDGraph.succ n g))) nodes in
+          let min_succ_count = List.fold_left min (List.hd succ_counts) succ_counts in
+          List.find (fun n -> List.length (NodeSet.elements (IDGraph.succ n g)) = min_succ_count) nodes
+        in
+        node_with_least_succ g nodes;
+    in
+    let start_node = get_start_block result_block_graph in
+     write_to_file("\n I am the starting node block: "^ (string_of_node start_node));
+
+    let ref_graph = ref IUGraph.empty in
+    let initLiveSet = ref NodeSet.empty in
+    (* Generate nodes and edges based on this block*)
+    let block_liveness_check (b : block) (ref_g : interfere_graph ref) (liveSet : NodeSet.t ref)=
+        (*iterate the inst from last to first, if meet an assignment statement like
+        x = y + z, create sets gen(y, z) kill(x), calculate Live set for next inst, create
+        edges for x *)
+        write_to_file("\n Process block : "^find_label(b)^"\n");
+        let build_edges (n : igraph_node) (liveSet : NodeSet.t) (ref_g : interfere_graph ref) =
+            (*build edges between n and all nodes in liveSet*)
+            List.iter (fun dst -> ref_g := specialAddEdge n dst !ref_g) (NodeSet.elements liveSet);
+        in
+        let liveness_check (instruction : inst) =
+            (* create gen and kill for current instruction *)
+            let gen = ref NodeSet.empty in
+            let kill = ref NodeSet.empty in
+            (match instruction with
+          (*  | Call(op) ->
+                List.iter (fun reg_number ->
+                    let gen_node = (reg_node_of_string reg_number) in
+                    gen := NodeSet.add gen_node !gen ;
+                     ) call_gen_list;
+                List.iter (fun reg_number ->
+                                    let kill_node = (reg_node_of_string reg_number) in
+                                    kill := NodeSet.add kill_node !kill ;
+                                     ) call_kill_list;*)
+            | Move(op1, op2)  (* x := y *) ->
+                (*build edges between op1 and all nodes still live right now*)
+                (match op1 with
+                | Var v  ->
+                    let cur_node = VarNode(v) in
+                    build_edges cur_node !liveSet ref_g;
+                    kill := NodeSet.add cur_node !kill
+                | Reg r  ->
+                    let cur_node = RegNode(r) in
+                    build_edges cur_node !liveSet ref_g;
+                    kill := NodeSet.add cur_node !kill
+                | _ -> () (*doesn't care *));
+                (match op2 with
+                | Var v  -> gen := NodeSet.add (VarNode(v)) !gen
+                | Reg r  -> gen := NodeSet.add (RegNode(r)) !gen
+                | _ -> () (*doesn't care *));
+            | Arith(op1, op2, _, op3)  (* x := y + z *) ->
+                (match op1 with
+                | Var v  ->
+                    let cur_node = VarNode(v) in
+                    build_edges cur_node !liveSet ref_g;
+                    kill := NodeSet.add cur_node !kill
+                | Reg r  ->
+                    let cur_node = RegNode(r) in
+                    build_edges cur_node !liveSet ref_g;
+                    kill := NodeSet.add cur_node !kill
+                | _ -> () (*doesn't care *));
+                (match op2 with
+                | Var v  -> gen := NodeSet.add (VarNode(v)) !gen
+                | Reg r  -> gen := NodeSet.add (RegNode(r)) !gen
+                | _ -> () (*doesn't care *));
+                (match op3 with
+                | Var v  -> gen := NodeSet.add (VarNode(v)) !gen
+                | Reg r  -> gen := NodeSet.add (RegNode(r)) !gen
+                | _ -> () (*doesn't care *));
+            | Load(op1, op2, _)  (* x := *(y+i) *) ->
+                (match op1 with
+                | Var v  ->
+                    let cur_node = VarNode(v) in
+                    build_edges cur_node !liveSet ref_g;
+                    kill := NodeSet.add cur_node !kill
+                | Reg r  ->
+                    let cur_node = RegNode(r) in
+                    build_edges cur_node !liveSet ref_g;
+                    kill := NodeSet.add cur_node !kill
+                | _ -> () (*doesn't care *));
+                (match op2 with
+                | Var v  -> gen := NodeSet.add (VarNode(v)) !gen
+                | Reg r  -> gen := NodeSet.add (RegNode(r)) !gen
+                | _ -> () (*doesn't care *));
+
+
+            | Store(op1, _, op2) (* *(x+i) := y *) ->
+                (match op1 with
+                | Var v  ->
+                    let cur_node = VarNode(v) in
+                    build_edges cur_node !liveSet ref_g;
+                | Reg r  ->
+                    let cur_node = RegNode(r) in
+                    build_edges cur_node !liveSet ref_g;
+                | _ -> () (*doesn't care *));
+                (match op2 with
+                | Var v  -> gen := NodeSet.add (VarNode(v)) !gen
+                | Reg r  -> gen := NodeSet.add (RegNode(r)) !gen
+                | _ -> () (*doesn't care *));
+
+            | If(op1, _, op2, _, _) (* if x < y then goto L1 else goto L2 *) ->
+                (match op1 with
+                | Var v  ->
+                    let cur_node = VarNode(v) in
+                    (*build_edges cur_node !liveSet ref_g;*)
+                    gen := NodeSet.add cur_node !gen
+                | Reg r  ->
+                    let cur_node = RegNode(r) in
+                    (*build_edges cur_node !liveSet ref_g;*)
+                    gen := NodeSet.add cur_node !gen
+                | _ -> () (*don't care *));
+                (match op2 with
+                | Var v  -> gen := NodeSet.add (VarNode(v)) !gen
+                | Reg r  -> gen := NodeSet.add (RegNode(r)) !gen
+                | _ -> () (*don't care *));
+
+            | _ -> (* write_to_file("\n Match a inst "^inst2string(instruction) ^"\n");*) () );
+            (*update liveSet*)
+            liveSet := NodeSet.diff !liveSet !kill;
+            liveSet := NodeSet.union !liveSet !gen;
+        in
+        List.iter liveness_check (List.rev b);
+        let node_name = find_label b in
+        add_to_node_set node_hash_table node_name !liveSet;
+    in
+
+    let liveOutSet = ref NodeSet.empty in
+    let visited = ref NodeSet.empty in
+    let rec dfs (f : func) (b : block) (result_block_graph : block_graph) (liveSet : NodeSet.t) =
+        let cur_node = VarNode((find_label b)) in
+        let liveOutSet = ref liveSet in
+        if not (NodeSet.mem cur_node !visited) then (
+            (*add to visited*)
+            visited := NodeSet.add cur_node !visited;
+            block_liveness_check b ref_graph liveOutSet;
+            (*check predecessors*)
+            List.iter (fun pre_node ->
+                match pre_node with
+                | VarNode(pre_node_label) ->
+                    let pre_block = label2block f pre_node_label in
+                    dfs f pre_block result_block_graph !liveOutSet
+             ) (NodeSet.elements (IDGraph.pred cur_node result_block_graph))
+             )
+        else
+            ();
+    in
+     List.iter (fun b ->
+        visited := NodeSet.empty;
+        dfs f b result_block_graph !liveOutSet) (List.rev f);
+
+    !ref_graph
--- assignment8/regalloc.ml
+++ assignment8/regalloc.ml
@@ -1,5 +1,6 @@
 open Mips
 open Cfg_ast
+open Cfg
 
 exception AllocError of string
 exception Implement_Me
@@ -19,6 +20,24 @@ let validreglist =
   R21 :: R22 :: R23 (*R24,R25 reserved*) :: R30 :: R31 :: []
 let regcount = List.length validreglist
 let validregset = reglist2sset validreglist 
+
+let string_of_colormap colormap =
+  let buffer = Buffer.create 100 in
+  NodeMap.iter (fun node regset ->
+    match regset with
+    | None -> Printf.bprintf buffer "%s -> unallocated\n" (Cfg.string_of_node node)
+    | Some reg -> Printf.bprintf buffer "%s -> %s\n" (Cfg.string_of_node node) (reg2string reg)
+  ) colormap;
+  Buffer.contents buffer
+
+
+let string_of_stack stack =
+  let buffer = Buffer.create 100 in
+  Stack.iter (fun node ->
+    Printf.bprintf buffer "Node: %s\n" (string_of_node node);
+  ) stack;
+  Buffer.contents buffer
+
 
 let rewriteOp colormap o =
   match o with
@@ -57,8 +76,131 @@ type assign_result =
 
  *)
 
+let is_register_node node =
+    match node with
+    | RegNode(reg) -> true
+    | VarNode(var_name) -> false
+
 let assign_colors (ig: Cfg.interfere_graph) f : assign_result =
-    raise Implement_Me
+    let ig_ref = ref ig in
+    let stack =  Stack.create() in
+    let bfs (ig: interfere_graph) (node : igraph_node) : igraph_node =
+        (* Breadth first search to find a node with degree less than regcount*)
+        let visited = ref NodeSet.empty in
+        let queue = Queue.create() in
+        let res_node = ref (VarNode("init")) in
+        Queue.add node queue;
+        visited := NodeSet.add node !visited;
+        while not (Queue.is_empty queue) do
+            let curNode = Queue.take queue in
+            let degree = IUGraph.degree curNode ig in
+            if degree < regcount then
+                (res_node := curNode);
+            List.map (fun neiNode ->
+                if not (NodeSet.mem neiNode !visited) then
+                    Queue.add neiNode queue;
+                    visited := NodeSet.add neiNode !visited;
+                    ) (NodeSet.elements (IUGraph.adj curNode ig));
+            ()
+        done;
+        !res_node;
+    in
+    let is_empty (ig: interfere_graph) =
+        if (List.length (NodeSet.elements (IUGraph.nodes ig))) = 0 then
+            true
+        else
+            false
+    in
+    let need_spill (ig: interfere_graph) =
+        let start_node = List.hd (NodeSet.elements (IUGraph.nodes ig)) in
+        let next_valid_node = bfs ig start_node in
+        match next_valid_node with
+            | VarNode("init") ->
+                true
+            | _ ->
+                false
+    in
+    let get_var_list (ig: Cfg.interfere_graph) : var list =
+        (*Find the node with largest degree, and return a var list*)
+        let node_ref = ref (List.hd (NodeSet.elements (IUGraph.nodes ig))) in
+        List.iter (fun node ->
+                let cur_degree = IUGraph.degree node ig in
+                let max_degree = IUGraph.degree !node_ref ig in
+                if (max_degree < cur_degree) then
+                    node_ref := node;
+            ) (NodeSet.elements (IUGraph.nodes ig));
+        let node_list = !node_ref::(NodeSet.elements (IUGraph.adj !node_ref ig)) in
+        let res_list =
+            List.map (fun node ->
+                    match node with
+                    | VarNode(var_name) -> var_name;
+                    | _ -> (* write_to_file("\n Fatal error 0001\n"); *) raise Implement_Me
+                ) node_list in
+        res_list
+    in
+    let build_colormap (ig: Cfg.interfere_graph) : colormap =
+      (*  write_to_file("inside build colormap stack is : "^string_of_stack(stack)^"\n");*)
+        let res_map = ref (NodeMap.empty : colormap) in
+        while not (Stack.is_empty stack) do
+            let cur_node = Stack.pop stack in
+            let available_reg = ref (reglist2sset validreglist) in
+            (*iterate all neighbor nodes of cur_node, and try to find in res_map, see if this neighbor node already assigned a color
+                if yes, mark that color as used, when finished all neighbor nodes, iterate validreglist to get a color that
+                has not been used
+            *)
+            List.iter (fun nei_node ->
+                    if NodeMap.mem nei_node !res_map  then
+                        (
+                            let rm_reg = (
+                                match NodeMap.find nei_node !res_map with
+                                | Some r -> r
+                            ) in
+                            available_reg := RegSet.remove rm_reg !available_reg;
+                        );
+                    if is_register_node nei_node then (
+                        let rm_reg = (
+                            match nei_node with
+                            | RegNode r -> r
+                        ) in
+                        available_reg := RegSet.remove rm_reg !available_reg;
+                    )
+                ) (NodeSet.elements (IUGraph.adj cur_node ig));
+            res_map := NodeMap.add cur_node (Some (List.hd (RegSet.elements !available_reg))) !res_map;
+
+        ()
+        done;
+        let msg_str = string_of_colormap !res_map in
+       (* write_to_file("In build colormap the map is :"^msg_str^"\n");*)
+        !res_map
+    in
+    let result = ref (Success((NodeMap.empty : colormap))) in
+
+    while (not (is_empty !ig_ref)) && (not (need_spill !ig_ref)) do
+        let start_node = List.hd (NodeSet.elements (IUGraph.nodes !ig_ref)) in
+        let next_valid_node = bfs !ig_ref start_node in
+        (*Push current node to stack*)
+        if not (is_register_node next_valid_node) then
+            Stack.push next_valid_node stack;
+        (*Remove current node from graph*)
+        ig_ref := IUGraph.rmNode next_valid_node !ig_ref;
+    done;
+    (* write_to_file("Reg alloc still alive 0002\n");*)
+    if (not (is_empty !ig_ref)) then
+        (*Need to return a fail var list*)
+        let var_list = get_var_list !ig_ref in
+        result := Fail(var_list);
+        (* write_to_file("Reg alloc still alive 0003\n");*)
+        !result
+    else
+        (*Need to build a color map*)
+        ((*write_to_file("before build colormap stack is : "^string_of_stack(stack)^"\n");*)
+        let res_map = build_colormap ig in
+        result := Success(res_map);
+         (* write_to_file("Reg alloc still alive 0004\n"); *)
+         let msg_str = string_of_colormap res_map in
+        write_to_file("The res_map is "^msg_str^"\n");
+        !result)
+
 
 let rec reg_alloc_spill (fraw : func) (sl: var list): func = 
   (*First spill all of the vars in sl by adding loads/stores to fraw*)
